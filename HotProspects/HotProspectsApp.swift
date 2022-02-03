950fe935e9286929f6a7873446ffa23b3f388ab5
Joanna <astalenczyk2@wp.pl>
2022-02-03 18:24:45 +0100

    first commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Joanna Staleńczyk on 02/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
